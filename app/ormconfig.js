f08080e960630ce60c753043ef67b0e3e967c8cd
Дмитрий Пасхин <d.pasxin@gmail.com>
2021-10-06 01:13:01 +0300

    fix(ormconfig): Add trailing comma

--- app/ormconfig.js
+++ app/ormconfig.js
@@ -6,5 +6,5 @@ module.exports = {
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
   synchronize: true,
-  entities: ['src/**/entities/*.ts']
+  entities: ['src/**/entities/*.ts'],
 }
