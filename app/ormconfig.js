20d9f99b43d1764452641404b61806c7d894dcaf
Дмитрий Пасхин <d.pasxin@gmail.com>
2021-10-06 00:45:51 +0300

    feat(skeleton): Set entities to typeORM and synchronize

--- app/ormconfig.js
+++ app/ormconfig.js
@@ -5,4 +5,6 @@ module.exports = {
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
+  synchronize: true,
+  entities: ['src/**/entities/*.ts']
 }
