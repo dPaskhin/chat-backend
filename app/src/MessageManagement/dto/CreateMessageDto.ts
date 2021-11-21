3e45aeb36e155a7a90c87059da22f312ac53ee2a
Дмитрий Пасхин <d.pasxin@gmail.com>
2021-11-21 23:18:03 +0300

    feat(message): Add more checks for create message dto

--- app/src/MessageManagement/dto/CreateMessageDto.ts
+++ app/src/MessageManagement/dto/CreateMessageDto.ts
@@ -1,9 +1,12 @@
-import { IsDefined } from 'class-validator';
+import { IsDefined, IsString, IsUUID } from 'class-validator';
 
 export class CreateMessageDto {
   @IsDefined({ message: 'Не передано значение сообщения' })
+  @IsString({ message: 'Сообщение должно быть строкой' })
   public value!: string;
 
   @IsDefined({ message: 'Не передан id комнаты' })
+  @IsString({ message: 'Id комнаты должно быть строкой' })
+  @IsUUID('all', { message: 'Id комнаты должно быть UUID' })
   public roomId!: string;
 }
